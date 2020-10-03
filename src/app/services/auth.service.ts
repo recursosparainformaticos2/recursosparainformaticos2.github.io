dbbb503487a51eaf349bc5c4bfbb87315e9e3f79
khrisparrales <lecristianot@gmail.com>
2020-10-03 15:27:33 -0500

    version 1.2 -fix edit

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -7,7 +7,7 @@ import { Router } from '@angular/router';
 })
 export class AuthService {
 
-  private URL = 'http://recursosinformaticos.herokuapp.com/api';
+  private URL = 'https://recursosinformaticos.herokuapp.com/api';
   constructor(private http: HttpClient, private router: Router) { }
 
   signUpUser(user) {
