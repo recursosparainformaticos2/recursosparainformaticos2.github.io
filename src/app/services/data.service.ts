3e1df0bef1aaf5eb4efb65bca2c09e4bd557fab2
Christopher Parrales <67471670+christopherparrales2017@users.noreply.github.com>
2021-03-04 11:22:11 -0500

    new db

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -12,7 +12,7 @@ import { catchError, tap } from 'rxjs/operators';
 })
 export class DataService {
   //selectrecurso:RootObject;
-  urlrecursos = 'https://recursosinformaticos.herokuapp.com/api/recursos'
+  urlrecursos = 'https://recursosinformaticos2.herokuapp.com/api/recursos'
   recursos: any
   recursos2: RootObject[]
   constructor(private http: HttpClient) {}
